092c0ff902c50863d42e74054efd2d2649840fa8
DaveyNavy <davidyzhu2@gmail.com>
2024-04-20 14:53:38 -0700

    Users can be added to clubs

--- backend/clubapp/views.py
+++ backend/clubapp/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, status
 from .serializers import ClubSerializer
 from .models import Club
+from rest_framework.response import Response
+
 # Create your views here.
-class SingleClubView(generics.RetrieveAPIView):
+class SingleClubView(generics.RetrieveUpdateAPIView):
     queryset = Club.objects.all()
     serializer_class = ClubSerializer
+
+class UpdateUserClubView(generics.UpdateAPIView):
+    queryset = Club.objects.all()
+    serializer_class = ClubSerializer
+
+    def update(self, request, pk):
+        instance = self.get_object()
+        instance.users.add(self.request.user)
+        return Response(status = status.HTTP_200_OK)
+           
+
+class UserClubsView(generics.ListAPIView):
+    serializer_class = ClubSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return Club.objects.filter(users=user)
