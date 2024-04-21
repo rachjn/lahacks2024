a998e3f4b2532bc1ed481186dbd7a21f67e58dd0
Rachel Jin <racheljin24@gmail.com>
2024-04-21 05:14:29 -0700

    commenting out

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -77,9 +77,9 @@ export default function Explore() {
   };
 
   // if (currentUser) {
-    return (
-      <div className="animate animate-fade duration-300 delay-75">
-        {/* <Navbar />
+  return (
+    <div className="animate animate-fade duration-300 delay-75">
+      {/* <Navbar />
         <div className="m-6 mt-0">Explore</div>
         <button
           onClick={() => handleClick()}
@@ -87,107 +87,78 @@ export default function Explore() {
         >
           Click to test
         </button> */}
-        <Navbar />
-        <div className="flex flex-col mt-16 items-center h-[28rem] mb-7 drop-shadow-md">
-          <div className="h-[28rem] w-4/5 bg-teal rounded-xl overflow-hidden drop-shadow-md">
-            <div
-              className={`absolute mx-5 h-[10rem] mt-[19.5rem] font-bold text-2xl " 
+      <Navbar />
+      <div className="flex flex-col mt-16 items-center h-[28rem] mb-7 drop-shadow-md">
+        <div className="h-[28rem] w-4/5 bg-teal rounded-xl overflow-hidden drop-shadow-md">
+          <div
+            className={`absolute mx-5 h-[10rem] mt-[19.5rem] font-bold text-2xl " 
             ${openBio ? "w-11/12 pr-3" : "visible"}`}
-            >
-              <div
-                className={`line-clamp-1
+          >
+            <div
+              className={`line-clamp-1
             ${openBio ? "-mt-[18.5rem] mb-2 line-clamp-none" : "visible"}`}
-              >
-                {clubName}
-              </div>
-              <div className="h-20 relative">
-                <div
-                  className={`mr-6 font-normal text-sm line-clamp-4
+            >
+              {clubName}
+            </div>
+            <div className="h-20 relative">
+              <div
+                className={`mr-6 font-normal text-sm line-clamp-4
             ${
               openBio
                 ? "absolute top-0 h-[23rem] line-clamp-none overflow-scroll"
                 : "visible"
             }`}
-                >
-                  {clubBio}
-                </div>
-                <Image
-                  className="cursor-pointer fixed bottom-5 right-5 animate animate-pulse"
-                  onClick={arrowClick}
-                  src="./arrow.svg"
-                  height={20}
-                  width={20}
-                  alt="arrow"
-                />
-                {/* <button className="absolute bottom-0 right-0 h-6 bg-white w-6 rounded-full "></button> */}
+              >
+                {clubBio}
               </div>
+              <Image
+                className="cursor-pointer fixed bottom-5 right-5 animate animate-pulse"
+                onClick={arrowClick}
+                src="./arrow.svg"
+                height={20}
+                width={20}
+                alt="arrow"
+              />
+              {/* <button className="absolute bottom-0 right-0 h-6 bg-white w-6 rounded-full "></button> */}
             </div>
-            <img
-              className={`drop-shadow-sm object-cover p-3 md:h-[20rem] lg:p-5 rounded-[2rem]
+          </div>
+          <img
+            className={`drop-shadow-sm object-cover p-3 md:h-[20rem] lg:p-5 rounded-[2rem]
             ${openBio ? "hidden" : "visible"}`}
-              src={image}
-              height={500}
-              width={500}
-              alt="club image"
-            />
-            <div
-              className={`w-full -z-10 absolute bottom-0 bg-gradient-to-t from-30% from-navy to-transparent h-[10rem]
+            src={image}
+            height={500}
+            width={500}
+            alt="club image"
+          />
+          <div
+            className={`w-full -z-10 absolute bottom-0 bg-gradient-to-t from-30% from-navy to-transparent h-[10rem]
             ${openBio ? "bg-navy h-full" : "visible"}`}
-            ></div>
-          </div>
-        </div>
-        <div className="flex justify-between mx-10 drop-shadow-lg mb-8">
-          <div className="cursor-pointer rounded-full h-[7rem] w-[7rem] bg-white flex justify-center">
-            <Image
-              src="./dislike.svg"
-              width={80}
-              height={80}
-              alt="dislike"
-              onClick={() => handleClick()}
-            />
-          </div>
-          <div className="cursor-pointer rounded-full h-[7rem] w-[7rem] bg-white flex justify-center">
-            <Image
-              src="./like.svg"
-              width={80}
-              height={80}
-              alt="like"
-              onClick={() => addUser()}
-            />
-          </div>
+          ></div>
         </div>
       </div>
-    );
-  // } else {
-    const router = useRouter();
-    return (
-      <div>
-        <Navbar />
-        <div className="text-xl flex justify-center">
-          You are not logged in!
+      <div className="flex justify-between mx-10 drop-shadow-lg mb-8">
+        <div className="cursor-pointer rounded-full h-[7rem] w-[7rem] bg-white flex justify-center">
+          <Image
+            src="./dislike.svg"
+            width={80}
+            height={80}
+            alt="dislike"
+            onClick={() => handleClick()}
+          />
         </div>
-
-        <div className="text-center mb-[3.4rem] mt-[3rem]">
-          <button
-            onClick={() => {
-              router.push("/");
-            }}
-            className="font-bold rounded-lg relative px-20 h-16 bg-navy mb-[1rem]"
-          >
-            Back to Home
-          </button>
+        <div className="cursor-pointer rounded-full h-[7rem] w-[7rem] bg-white flex justify-center">
+          <Image
+            src="./like.svg"
+            width={80}
+            height={80}
+            alt="like"
+            onClick={() => addUser()}
+          />
         </div>
-      </div>
-      
-      <div className="text-center mb-[3.4rem] mt-[3rem]">
-        <button
-          onClick={() => {router.push("/");}}
-          className="font-bold rounded-lg relative px-20 h-16 bg-navy mb-[1rem]"
-        >
-          Back to Home
-        </button>
       </div>
     </div>
-    )
+  );
+  // } else {
+
   // }
 }
