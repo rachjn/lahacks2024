deb518e2f470bc3f416f9232d6be1f89de4bb2bb
DaveyNavy <davidyzhu2@gmail.com>
2024-04-21 02:16:19 -0700

    Added profile

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -10,7 +10,8 @@ const client = axios.create({
 });
 
 export default function Profile() {
-  const [context, setContext] = useState("");
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
   const [data, setData] = useState("");
   const [myName, setMyName] = useState("");
   const [parsedUserData, setParsedUserData] = useState("");
@@ -29,8 +30,12 @@ export default function Profile() {
   client
     .get("/api/user")
     .then((response) => {
+      return response.data.user;
+    })
+    .then((data) => {
       // Handle successful response
-      setContext(JSON.stringify(response.data));
+      setUserName(data.username);
+      setEmail(data.email);
     })
     .catch((error) => {
       // Handle error
@@ -84,7 +89,8 @@ export default function Profile() {
       <div className="flex items-center justify-center gap-5 mt-10">
         <div className="rounded-full bg-black relative w-[136px] h-[136px]"></div>
         <div className="w-[160px] h-[120px] text-lg">
-          <div className="text-3xl font-bold">{}</div>
+          <div className="text-3xl font-bold">{userName}</div>
+          {email}
         </div>
       </div>
       <div className="m-10 mt-14 font-bold text-3xl">
@@ -95,8 +101,6 @@ export default function Profile() {
           <div key={index}>{club.name} </div>
         ))}
       </div>
-
-      <p>Context: {context}</p>
     </div>
   );
 }
